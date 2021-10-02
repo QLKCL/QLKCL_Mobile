4c67937a0241e1d1f5e0b7a56c188860c34a131a
lesonlhld <lesonlhld@gmail.com>
2021-10-02 22:33:30 +0700

    Init project

---
+++ runapp.bat
@@ -0,0 +1,9 @@
+echo "Starting app........................................."
+
+flutter run
+
+@REM flutter pub get
+@REM flutter pub run flutter_launcher_icons:main
+
+@REM flutter doctor
+@REM flutter doctor --android-licenses
