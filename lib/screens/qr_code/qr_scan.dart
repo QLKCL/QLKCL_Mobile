66ea9df140eaa16ce9f9c4ab25a75f6ad13ad2c1
lesonlhld <lesonlhld@gmail.com>
2021-10-26 14:22:02 +0700

    update new qr scan

--- lib/screens/qr_code/qr_scan.dart
+++ lib/screens/qr_code/qr_scan.dart
@@ -1,93 +1,132 @@
-import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
 
-import 'package:barcode_scan2/barcode_scan2.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-//cre: https://pub.dev/packages/barcode_scan2
-
+// cre: https://pub.dev/packages/qr_code_scanner/example
 class QrCodeScan extends StatefulWidget {
-  const QrCodeScan({Key? key}) : super(key: key);
   @override
-  _QrCodeScanState createState() => _QrCodeScanState();
+  State<StatefulWidget> createState() => _QrCodeScanState();
 }
 
 class _QrCodeScanState extends State<QrCodeScan> {
-  ScanResult? scanResult;
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
 
-  static final _possibleFormats = BarcodeFormat.values.toList()
-    ..removeWhere((e) => e == BarcodeFormat.unknown);
-
-  List<BarcodeFormat> selectedFormats = [..._possibleFormats];
-
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
   @override
-  void initState() {
-    super.initState();
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
   }
 
   @override
   Widget build(BuildContext context) {
-    final scanResult = this.scanResult;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Quét mã QR'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.flash_on),
-            tooltip: 'Flash',
-            onPressed: () {},
+            icon: FutureBuilder(
+              future: controller?.getFlashStatus(),
+              builder: (context, snapshot) {
+                if (snapshot.data == false)
+                  return const Icon(Icons.flash_on);
+                else
+                  return const Icon(Icons.flash_off);
+              },
+            ),
+            tooltip: 'Toggle Flash',
+            onPressed: () async {
+              await controller?.toggleFlash();
+              setState(() {});
+            },
           )
         ],
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
+      body: Column(
         children: <Widget>[
-          if (scanResult != null)
-            Card(
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    title: const Text('Raw Content'),
-                    subtitle: Text(scanResult.rawContent),
+          Expanded(flex: 7, child: _buildQrView(context)),
+          Expanded(
+            flex: 3,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              children: <Widget>[
+                if (result != null)
+                  Card(
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: const Text('Type'),
+                          subtitle: Text(describeEnum(result!.format)),
+                        ),
+                        ListTile(
+                          title: const Text('Content'),
+                          subtitle: Text(result!.code),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
+              ],
             ),
-          Container(
-            margin: const EdgeInsets.all(16),
-            child: ElevatedButton(
-              onPressed: () {
-                _scan();
-              },
-              child: Text(
-                'Quét mã',
-              ),
-            ),
-          ),
+          )
         ],
       ),
     );
   }
 
-  Future<void> _scan() async {
-    try {
-      final result = await BarcodeScanner.scan(
-        options: ScanOptions(
-          restrictFormat: selectedFormats,
-        ),
-      );
-      setState(() => scanResult = result);
-    } on PlatformException catch (e) {
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
       setState(() {
-        scanResult = ScanResult(
-          type: ResultType.Error,
-          format: BarcodeFormat.unknown,
-          rawContent: e.code == BarcodeScanner.cameraAccessDenied
-              ? 'The user did not grant the camera permission!'
-              : 'Unknown error: $e',
-        );
+        result = scanData;
       });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('no Permission')),
+      );
     }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
