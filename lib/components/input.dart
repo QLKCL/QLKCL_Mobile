5ef3303d17d0a9804741315ff37ab43a4b93f1c6
lesonlhld <lesonlhld@gmail.com>
2021-11-02 22:00:25 +0700

    finish input component

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -8,6 +8,13 @@ class Input extends StatefulWidget {
   final TextInputType type;
   final bool enabled;
   final String? initValue;
+  final String? helper;
+  final bool showClearButton;
+  final TextEditingController? controller;
+  final int? maxLength;
+  final String? Function(String?)? validatorFunction;
+  final void Function(String)? onChangedFunction;
+  final void Function(String?)? onSavedFunction;
 
   Input(
       {Key? key,
@@ -17,7 +24,14 @@ class Input extends StatefulWidget {
       this.required: false,
       this.type: TextInputType.text,
       this.enabled: true,
-      this.initValue})
+      this.initValue,
+      this.helper,
+      this.showClearButton = true,
+      this.controller,
+      this.maxLength,
+      this.validatorFunction,
+      this.onChangedFunction,
+      this.onSavedFunction})
       : super(key: key);
 
   @override
@@ -34,32 +48,26 @@ class _InputState extends State<Input> {
       child: TextFormField(
         obscureText: widget.obscure,
         keyboardType: widget.type,
-        // onSaved: (newValue) => password = newValue,
-        initialValue: widget.initValue,
-        onChanged: (value) {
-          // if (value.isNotEmpty) {
-          //   removeError(error: kPassNullError);
-          // } else if (value.length >= 8) {
-          //   removeError(error: kShortPassError);
-          // }
-          return null;
-        },
-        validator: (value) {
-          // if (value!.isEmpty) {
-          //   addError(error: kPassNullError);
-          //   return "";
-          // } else if (value.length < 8) {
-          //   addError(error: kShortPassError);
-          //   return "";
-          // }
-          return null;
-        },
+        onSaved: widget.onSavedFunction,
+        initialValue: widget.controller == null ? widget.initValue : null,
+        onChanged: widget.onChangedFunction,
+        validator: widget.validatorFunction,
         enabled: widget.enabled,
+        controller: widget.controller,
+        maxLength: widget.maxLength,
         decoration: InputDecoration(
-          labelText: widget.required ? widget.label + " \*" : widget.label,
-          hintText: widget.hint,
-        ),
-        
+            labelText: widget.required ? widget.label + " \*" : widget.label,
+            hintText: widget.hint,
+            suffixIcon: (widget.showClearButton && widget.controller != null)
+                ? IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      widget.controller!.clear();
+                    },
+                  )
+                : null,
+            helperText: widget.helper,
+            errorText: errors.isEmpty ? null : "error"),
       ),
     );
   }
