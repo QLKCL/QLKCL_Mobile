860db50fbdfd1bed3ffb2c8ad7b4a5d0a387414e
lesonlhld <lesonlhld@gmail.com>
2022-01-25 20:28:16 +0700

    add flutter_speed_dial

--- lib/screens/home/manager_home_screen.dart
+++ lib/screens/home/manager_home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:qlkcl/config/app_theme.dart';
 import 'package:qlkcl/models/key_value.dart';
 import 'package:qlkcl/networking/api_helper.dart';
@@ -11,6 +11,8 @@ import 'package:qlkcl/utils/constant.dart';
 import 'package:intl/intl.dart';
 import 'package:badges/badges.dart';
 
+// cre: https://pub.dev/packages/flutter_speed_dial/example
+
 class ManagerHomePage extends StatefulWidget {
   static const String routeName = "/manager_home";
   ManagerHomePage({Key? key}) : super(key: key);
@@ -21,6 +23,10 @@ class ManagerHomePage extends StatefulWidget {
 
 class _ManagerHomePageState extends State<ManagerHomePage> {
   late Future<dynamic> futureData;
+
+  var renderOverlay = true;
+  var useRAnimation = true;
+  var isDialOpen = ValueNotifier<bool>(false);
 
   @override
   void initState() {
@@ -38,133 +44,124 @@ class _ManagerHomePageState extends State<ManagerHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Trang chủ"),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            showBarModalBottomSheet(
-              barrierColor: Colors.black54,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0)),
+    return WillPopScope(
+        onWillPop: () async {
+          if (isDialOpen.value) {
+            isDialOpen.value = false;
+            return false;
+          }
+          return true;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Trang chủ"),
+            centerTitle: true,
+            actions: [
+              Badge(
+                position: BadgePosition.topEnd(top: 0, end: 3),
+                animationDuration: Duration(milliseconds: 300),
+                animationType: BadgeAnimationType.scale,
+                badgeContent: Text(
+                  "",
+                  style: TextStyle(color: CustomColors.white),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.notifications_none_outlined),
+                  onPressed: () {},
+                ),
               ),
-              useRootNavigator: true,
-              context: context,
-              builder: (context) {
-                // Using Wrap makes the bottom sheet height the height of the content.
-                // Otherwise, the height will be half the height of the screen.
-                return Wrap(
-                  children: [
-                    ListTile(
-                      title: Center(
-                        child: Text(
-                          'Tạo mới',
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                      ),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.description_outlined),
-                      title: Text('Phiếu xét nghiệm'),
-                      onTap: () {
-                        Navigator.pushReplacementNamed(
-                            context, AddTest.routeName);
-                      },
-                    ),
-                    Divider(),
-                    ListTile(
-                      leading: Icon(Icons.person_add_alt),
-                      title: Text('Người cách ly'),
-                      onTap: () {
-                        Navigator.pushReplacementNamed(
-                            context, AddMember.routeName);
-                      },
-                    ),
-                    Divider(),
-                    ListTile(
-                      leading: Icon(Icons.manage_accounts_outlined),
-                      title: Text('Quản lý'),
-                    ),
-                    Divider(),
-                    ListTile(
-                      leading: Icon(Icons.business_outlined),
-                      title: Text('Khu cách ly'),
-                      onTap: () {
-                        Navigator.pushReplacementNamed(
-                            context, NewQuarantine.routeName);
-                      },
-                    ),
-                  ],
-                );
+            ],
+          ),
+          body: RefreshIndicator(
+            onRefresh: () => Future.sync(() {
+              setState(() {
+                futureData = fetch();
+              });
+            }),
+            child: FutureBuilder<dynamic>(
+              future: futureData,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return InfoManagerHomePage(
+                    waitingUsers: snapshot.data['number_of_waiting_users'],
+                    suspectedUsers: snapshot.data['number_of_suspected_users'],
+                    needTestUsers: snapshot.data['number_of_need_test_users'],
+                    canFinishUsers: snapshot.data['number_of_can_finish_users'],
+                    waitingTests: snapshot.data['number_of_waiting_tests'],
+                    numberIn: snapshot.data['in'].entries
+                        .map((entry) => KeyValue(
+                            id: DateFormat("dd/MM/yyyy")
+                                .format(DateTime.parse(entry.key)),
+                            name: entry.value))
+                        .toList()
+                        .cast<KeyValue>()
+                        .reversed
+                        .toList(),
+                    numberOut: snapshot.data['out'].entries
+                        .map((entry) => KeyValue(
+                            id: DateFormat("dd/MM/yyyy")
+                                .format(DateTime.parse(entry.key)),
+                            name: entry.value))
+                        .toList()
+                        .cast<KeyValue>()
+                        .reversed
+                        .toList(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+
+                // By default, show a loading spinner.
+                // return const CircularProgressIndicator();
+                return InfoManagerHomePage();
               },
-            );
-          },
-          icon: Icon(Icons.add_box_outlined),
-        ),
-        actions: [
-          Badge(
-            position: BadgePosition.topEnd(top: 0, end: 3),
-            animationDuration: Duration(milliseconds: 300),
-            animationType: BadgeAnimationType.scale,
-            badgeContent: Text(
-              "",
-              style: TextStyle(color: CustomColors.white),
-            ),
-            child: IconButton(
-              icon: Icon(Icons.notifications_none_outlined),
-              onPressed: () {},
             ),
           ),
-        ],
-      ),
-      body: RefreshIndicator(
-        onRefresh: () => Future.sync(() {
-          setState(() {
-            futureData = fetch();
-          });
-        }),
-        child: FutureBuilder<dynamic>(
-          future: futureData,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return InfoManagerHomePage(
-                waitingUsers: snapshot.data['number_of_waiting_users'],
-                suspectedUsers: snapshot.data['number_of_suspected_users'],
-                needTestUsers: snapshot.data['number_of_need_test_users'],
-                canFinishUsers: snapshot.data['number_of_can_finish_users'],
-                waitingTests: snapshot.data['number_of_waiting_tests'],
-                numberIn: snapshot.data['in'].entries
-                    .map((entry) => KeyValue(
-                        id: DateFormat("dd/MM/yyyy")
-                            .format(DateTime.parse(entry.key)),
-                        name: entry.value))
-                    .toList()
-                    .cast<KeyValue>()
-                    .reversed
-                    .toList(),
-                numberOut: snapshot.data['out'].entries
-                    .map((entry) => KeyValue(
-                        id: DateFormat("dd/MM/yyyy")
-                            .format(DateTime.parse(entry.key)),
-                        name: entry.value))
-                    .toList()
-                    .cast<KeyValue>()
-                    .reversed
-                    .toList(),
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
+          floatingActionButton: SpeedDial(
+            icon: Icons.add,
+            activeIcon: Icons.close,
+            spacing: 3,
+            openCloseDial: isDialOpen,
+            childPadding: const EdgeInsets.all(5),
+            spaceBetweenChildren: 4,
+            
+            /// If false, backgroundOverlay will not be rendered.
+            renderOverlay: renderOverlay,
+            overlayColor: Colors.black,
+            overlayOpacity: 0.3,
+            useRotationAnimation: useRAnimation,
+            tooltip: 'Tạo mới',
 
-            // By default, show a loading spinner.
-            // return const CircularProgressIndicator();
-            return InfoManagerHomePage();
-          },
-        ),
-      ),
-    );
+            animationSpeed: 200,
+            shape: const StadiumBorder(),
+            childMargin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            children: [
+              SpeedDialChild(
+                child: const Icon(Icons.description_outlined),
+                label: 'Phiếu xét nghiệm',
+                onTap: () => Navigator.of(context, rootNavigator: true)
+                    .pushNamed(AddTest.routeName),
+              ),
+              SpeedDialChild(
+                child: const Icon(Icons.person_add_alt),
+                label: 'Người cách ly',
+                onTap: () => Navigator.of(context, rootNavigator: true)
+                    .pushNamed(AddMember.routeName),
+              ),
+              SpeedDialChild(
+                child: const Icon(Icons.manage_accounts_outlined),
+                label: 'Quản lý',
+                onTap: () => {},
+              ),
+              SpeedDialChild(
+                child: const Icon(Icons.business_outlined),
+                label: 'Khu cách ly',
+                visible: true,
+                onTap: () => Navigator.of(context, rootNavigator: true)
+                    .pushNamed(NewQuarantine.routeName),
+              ),
+            ],
+          ),
+        ));
   }
 }
